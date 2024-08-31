b2877ecd83f70aa5b97351bdca1067abf413982e
Ariel Ferreyra <arielgerman293@gmail.com>
2024-08-30 23:19:18 -0300

    se agregaro botones en el layout

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export * from './Navbar';
+export * from './layout';
+export * from './ItemListConteiner';
+export * from './CartWidget';
--- src/components/layout/MainLayout.jsx
+++ src/components/layout/MainLayout.jsx
@@ -1,12 +1,24 @@
+import { useReducedMotion } from "framer-motion";
 import Navbar from "../NavBar/NavBar";
+import { useRef } from "react";
 
 
 const MainLayout = ({children}) =>{
+    const footerDivRef = useRef();
+
+const handleClick =() =>{
+    footerDivRef.current.innerHTML = "Footer Content Reference";
+    footerDivRef.current.setAttribute = ("class", "footer-new")
+
+}
+
+
     return(
         <div>
             <Navbar/>
             {children}
-            <div><footer></footer></div>
+            <div ref={footerDivRef}>footer</div>
+            <button onClick={handleClick}>Click</button>
         </div>
     )
 };
